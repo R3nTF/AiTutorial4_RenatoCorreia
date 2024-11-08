96c213e4300600c20419c258ecfca5b53f01ed01
R3nTF <148765115+R3nTF@users.noreply.github.com>
2024-11-08 19:14:53 +0000

    State Class

---
+++ Assets/Part2/State.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE
+    {
+        IDLE, PATROL, PURSUE, ATTACK, SLEEP
+    };
+
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+}
